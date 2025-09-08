fe765b6768f1c5186b2c12372d2c24dc372e3250
Frankie Roberto <frankie@frankieroberto.com>
2025-09-08 21:47:14 +0100

    Updates for v8 of plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,13 @@ export default function(eleventyConfig) {
 
   // Register the plugin
   eleventyConfig.addPlugin(govukEleventyPlugin, {
+    templates: {
+      feed: {
+        title: 'NHS notes'
+      },
+      tags: false,
+      searchIndex: false
+    },
     header: {
       logotype: {
         text: 'NHS notes'
@@ -35,7 +42,6 @@ export default function(eleventyConfig) {
     },
     themeColor: '#005eb8',
     stylesheets: ['/assets/styles.css'],
-    feedUrl: 'posts/feed.xml',
     titleSuffix: 'NHS notes from Frankie Roberto',
     url: process.env.GITHUB_ACTIONS && 'https://frankieroberto.github.io/nhsnotes/'
   })
